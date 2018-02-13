b09ca98d95664d46eb1a58fdb3332f307e757dae
OmerFlame <renginald.bagel@gmail.com>
2018-02-13 10:29:43 +0200

    Progress stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_ONuguXjB0IYkqowkUzJYZCjWt0rcEZo",
+  authDomain: "http://localhost:4200",
+  databaseURL: "https://twofactorauthjs.firebaseio.com/",
+  storageBucket: "gs://twofactorauthjs.appspot.com",
+  messagingSenderID: "197504538508"
+}
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +22,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    MatButtonModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
